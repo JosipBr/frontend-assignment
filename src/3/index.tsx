f1fa99e65d644accf2c07c91ccd3cddc89e29bd6
JosipBr <92014240+JosipBr@users.noreply.github.com>
2022-06-07 11:30:42 +0200

    Update index.tsx

--- src/3/index.tsx
+++ src/3/index.tsx
@@ -2,15 +2,34 @@ import { FunctionComponent, useState } from "react";
 
 const Task3: FunctionComponent = () => {
   const [inputValue, setInputValue] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+  ]);
 
   return (
     <div>
       <label>Search Input: </label>
-      <input />
+      <input type="text" placeholder="search..." onChange={event => {setInputValue(event.target.value)}} />
       <br />
       <br />
-      #List goes here#
+      {items.filter((val) => {
+        if (inputValue == "")
+          {
+            return <div>{items.map(i => <li>{i}</li>)}</div>
+          }
+        else if(val.toLowerCase().includes(inputValue.toLowerCase()))
+          {
+            return <div>{items.map(i => <li>{i}</li>)}</div>
+          }
+      })}
+          
+        
+      
+        
     </div>
   );
 };
